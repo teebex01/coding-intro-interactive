e06eb9770f83f132f5f9cca53978c68f67b1b9ca
oluwagbenro afuwape <tobby2286@gmail.com>
2022-06-22 10:34:05 -0700

    First Commit

---
+++ making-decisions-demo/varsAndConditionals.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+let jonSnowAttack= 25;
+let jamieLannisAttack= 35;
+
+if (jonSnowAttack > jamieLannisAttack ){
+    console.log(`jon Snow has better attack than Jamie`)
+} else if (jamieLannisAttack > jonSnowAttack) {
+    console.log(`Jamie has a betterr attack than Jon`)
+} else {
+    console.log(`Jon and jamie have the same attack`)
+}
+//
